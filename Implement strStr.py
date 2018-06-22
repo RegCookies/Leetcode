4eb083d234835edd6ecc868f6b33921b5d52465c
Regcookie <RegCookies@users.noreply.github.com>
2018-06-22 15:34:11 +1000

    Add files via upload

---
+++ Implement strStr.py
@@ -0,0 +1,10 @@
+def strStr(haystack, needle):
+    """
+    :type haystack: str
+    :type needle: str
+    :rtype: int
+    """
+    if needle in haystack:
+        return haystack.index(needle)
+    else:
+        return -1

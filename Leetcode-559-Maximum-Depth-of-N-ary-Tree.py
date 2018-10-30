8fa6cabaaf2a467f4e5dd4f010c7fe5cb5478ae5
Regcookie <RegCookies@users.noreply.github.com>
2018-10-30 08:43:28 +0800

    Add files via upload

---
+++ Leetcode-559-Maximum-Depth-of-N-ary-Tree.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.children:
+            return 1 
+        return max(self.maxDepth(node) for node in root.children )+1

bb1280552b823149797b4e18546bb331cc2f756d
Regcookie <RegCookies@users.noreply.github.com>
2018-10-30 16:17:04 +0800

    Add files via upload

---
+++ Leetcode-500-Keyboard-Row.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        
+        line1="qwertyuiop"
+        line2="asdfghjkl"
+        line3="zxcvbnm"
+        ans=[]
+        for word in words:
+            temp = word.lower()
+            if (self.findWord(temp, line1)):
+                ans.append(word)
+            elif(self.findWord(temp,line2)):
+                ans.append(word)
+            elif(self.findWord( temp, line3)):
+                ans.append(word)
+        return ans  
+    def findWord(self, word,base):
+        for char in word:
+            if char not in base:
+                return False
+        return True       

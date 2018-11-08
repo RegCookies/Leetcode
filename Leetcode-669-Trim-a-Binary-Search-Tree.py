e54679261bd01bd60b86f58be7d30ea86b58c684
Regcookie <RegCookies@users.noreply.github.com>
2018-11-09 08:53:25 +1100

    Add files via upload

---
+++ Leetcode-669-Trim-a-Binary-Search-Tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        def trim(node):
+            if not node:
+                return None
+            elif node.val > R:
+                return trim(node.left)
+            elif node.val < L:
+                return trim(node.right)
+            else:
+                node.left = trim(node.left)
+                node.right = trim(node.right)
+                return node
+
+        return trim(root)

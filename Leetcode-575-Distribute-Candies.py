503ff3471ce3a983f67ac2802f852dbad421b8bf
Regcookie <RegCookies@users.noreply.github.com>
2018-11-09 08:58:03 +1100

    Add files via upload

---
+++ Leetcode-575-Distribute-Candies.py
@@ -0,0 +1,8 @@
+class Solution:
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        return int(min(len(candies)/2,len(set(candies))))
+        

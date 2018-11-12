782a9f1dbdf2aaee5e9e40b4f1a24a373544f2ef
Regcookie <RegCookies@users.noreply.github.com>
2018-11-12 15:32:06 +1100

    Add files via upload

---
+++ Leetcode-496-Next-Greater-Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        r_list = []
+        for item in nums1:
+	        r_list.append(next((j for j in nums2[nums2.index(item):] if j > item),-1))
+        return r_list

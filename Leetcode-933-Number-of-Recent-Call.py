8f03e96b72bf81c2e7c54322900232b55508ba82
Regcookie <RegCookies@users.noreply.github.com>
2018-12-17 22:34:48 +0800

    Add files via upload

---
+++ Leetcode-933-Number-of-Recent-Call.py
@@ -0,0 +1,13 @@
+def __init__(self):
+        self.q = collections.deque() 
+
+    def ping(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        self.q.append(t)
+        while self.q[0] < t-3000:
+            self.q.popleft()
+        return len(self.q)
+      
